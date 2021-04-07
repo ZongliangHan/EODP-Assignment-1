ac00a356c8a749e98c1e21c54cc215c14a6db320
Harry Han <80336811+ZongliangHan@users.noreply.github.com>
2021-04-07 16:10:25 +0800

    Update partb2.py

--- assignment-1-ZongliangHan-master/partb2.py
+++ assignment-1-ZongliangHan-master/partb2.py
@@ -3,3 +3,19 @@ import re
 import os
 import sys
 
+#This is the function for pre_processing
+def prepocessing (string):
+    pattern = r'[^a-zA-Z]'
+    string = re.sub(pattern, r' ', string)
+    string = re.sub(r' +', r' ', string)
+    string = string.lower()
+    return string
+
+#pre_processing the the given article
+txt = sys.argv[1]
+f=open('cricket/'+txt[7:], 'r')
+string = f.read()
+string = prepocessing (string)
+print (string)
+
+        
